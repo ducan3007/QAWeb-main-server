6afaa9e3b9dc64cbb3eb5205f24c9bf04449f85f
ducan3007 <58422724+ducan3007@users.noreply.github.com>
2021-09-23 22:24:35 +0700

    update user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-module.exports = mongoose.model('user, userModel')
+ const userModel = new Schema({
+    username: { type: String, required : true, unique : true},  
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birth: {type: String, required:false},
+    profileAvatar:{type: String, default : function(){
+        return `https://www.gravatar.com/avatar/${this._id}?s=32&d=identicon&r=PG`
+    }}
+
+    
+ })
+    
+console.log(userModel.path('_id'));
+
+const user = mongoose.model('user', userModel)
