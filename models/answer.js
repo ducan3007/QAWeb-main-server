2ee7ea0da9e720de4d51b91da92f49469ee1a84c
ducan3007 <58422724+ducan3007@users.noreply.github.com>
2021-09-29 22:51:52 +0700

    update models

--- models/answer.js
+++ models/answer.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = require('./comment');
-
+const voteSchema = require('./vote');
 const answerSchema = new Schema({
     author:{
-        type:Schema.Types.ObjectId;
-        required:true;
+        type:Schema.Types.ObjectId,
+        required:true,
     },
-    text:{
+    content:{
         type:String,
-    }
+    },
     created:{
         type:Date,
-        default: Date
-    }
+        default: Date.now
+    },
+    votes:[voteSchema],
+    comments:[commentSchema]
 })
-module.exports = mongoose.model('answer', userModel)
+module.exports = answerSchema;
